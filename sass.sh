8e0a59a7a889d22f5dbdeb096fad54ba597a6427
tdascoli <thomas@dasco.li>
2019-11-06 22:27:16 +0100

    changes

---
+++ sass.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+sass src/scss/unigrid.scss:docs/unigrid/unigrid.css
+sass src/scss/generator.scss:docs/unigrid-generator.css
+
+sass src/scss/example.scss:docs/example.css
+
+sass src/scss/unigrid-core.scss:docs/unigrid/unigrid-core.css
+sass src/scss/unigrid-forms.scss:docs/unigrid/unigrid-forms.css
+sass src/scss/unigrid-grid.scss:docs/unigrid/unigrid-grid.css
+sass src/scss/unigrid-piktos.scss:docs/unigrid/unigrid-piktos.css
+sass src/scss/unigrid-typography.scss:docs/unigrid/unigrid-typography.css
